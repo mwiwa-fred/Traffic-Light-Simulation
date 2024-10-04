18778b9e5d1f5b6bd70a21cd73eadecf98262e8e
Elvis-Munene <104641808+Elvis-Munene@users.noreply.github.com>
2024-10-04 08:27:48 +0300

    updates the task

--- TrafficLightSimulation.ino
+++ TrafficLightSimulation.ino
@@ -1,9 +1,9 @@
 // Traffic Light Simulation using ESP32
 
 // Pin definitions
-const int redLightPin = 18;   // Red LED connected to GPIO 23
-const int yellowLightPin = 22; // Yellow LED connected to GPIO 22
-const int greenLightPin = 21;  // Green LED connected to GPIO 21
+const int redLightPin = ;   //Pick a GPIO pin for Red.
+const int yellowLightPin = ; // Pick a GPIO pin for Yellow.
+const int greenLightPin = ;  // Pick a GPIO pin for Green.
 
 void setup() {
   // Initialize the LED pins as outputs
@@ -17,18 +17,15 @@ void loop() {
   digitalWrite(redLightPin, HIGH);  // Turn on red light
   digitalWrite(yellowLightPin, LOW); // Turn off yellow light
   digitalWrite(greenLightPin, LOW);  // Turn off green light
-  delay(5000);                       // Wait for 5 seconds
 
   // Green light for 5 seconds
   digitalWrite(redLightPin, LOW);   // Turn off red light
   digitalWrite(yellowLightPin, LOW); // Turn off yellow light
   digitalWrite(greenLightPin, HIGH); // Turn on green light
-  delay(5000);                       // Wait for 5 seconds
 
   // Yellow light for 2 seconds
   digitalWrite(redLightPin, LOW);   // Turn off red light
   digitalWrite(yellowLightPin, HIGH); // Turn on yellow light
   digitalWrite(greenLightPin, LOW);  // Turn off green light
-  delay(2000);                       // Wait for 2 seconds
 }
 
